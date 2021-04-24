41808893825c09177e1be7d7d8c27f1f97cebba6
maksbulova <maks.bulova@gmail.com>
2021-04-24 10:42:57 +0300

    Lamp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,91 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public enum ControllType
-    {
-        racing,
-        fps,
-        tps
-    }
 
     [Header("Параметры игрока")]
-    public ControllType controllType;
-    public float moveSpeed, rotateSpeed;
+    public float moveSpeed;
+    public float rotateSpeed;
 
-    [Header("Параметры управления"), Space]
-    public bool moveWorldAxis;
-    public bool mouseVertical;
+    private Rigidbody rb;
 
     private void Awake()
     {
-        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        // чтоб не опрокидывался
+        // rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
+
+
 
     private void FixedUpdate()
     {
-        switch (controllType)
+
+        // движение
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(hor, 0, vert).normalized * moveSpeed * Time.fixedDeltaTime;
+        move = transform.TransformDirection(move);
+        rb.MovePosition(transform.position + move);
+
+
+        // вращение
+        if (!Input.GetMouseButton(0) && !Input.GetMouseButton(1))
         {
-            case ControllType.racing:
-                RaceControll();
-                break;
-            case ControllType.fps:
-                FpsControll();
-                break;
-            case ControllType.tps:
-                TpsControll();
-                break;
-            default:
-                break;
+            float mouseHor = Input.GetAxis("Mouse X") * rotateSpeed * Time.fixedDeltaTime;
+            Vector3 rotateAngle = new Vector3(0, rotateSpeed, 0);
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(mouseHor * rotateAngle * Time.deltaTime));
+
         }
     }
 
-    private void RaceControll()
-    {
-        float vert = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
-        float hor = Input.GetAxis("Horizontal") * rotateSpeed * Time.fixedDeltaTime;
-
-        transform.Translate(0, 0, vert);
-        transform.Rotate(0, hor, 0);
-    }
-
-    private void FpsControll()
-    {
-        float vert = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
-        float hor = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
-
-        float mouseVert = 0;
-        if (mouseVertical)
-        {
-            mouseVert = Input.GetAxis("Mouse Y") * rotateSpeed * Time.fixedDeltaTime;
-        }
-        float mouseHor = Input.GetAxis("Mouse X") * rotateSpeed * Time.fixedDeltaTime;
-
-        Space space = moveWorldAxis ? Space.World : Space.Self;
-        transform.Translate(hor, 0, vert, space);
-
-        // выглядит странно но решает проблему шарнирного застревания, не знаю как в кватернионах поворачивать в мировом пространстве
-        transform.Rotate(0, mouseHor, 0, Space.World);
-        transform.Rotate(-mouseVert, 0, 0, Space.Self);
-        
-        // transform.rotation *= Quaternion.Euler(mouseVert, mouseHor, 0); 
-
-        Debug.DrawRay(transform.position, transform.forward * 2, Color.red);
-    }
-
-    private void TpsControll()
-    {
-        float vert = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
-        float hor = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
-
-        Space space = moveWorldAxis ? Space.World : Space.Self;
-        transform.Translate(hor, 0, vert, space);
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, hitInfo: out RaycastHit hit, maxDistance: 100);
-
-        Vector3 dir = (hit.point - transform.position).normalized;
-        transform.rotation = Quaternion.LookRotation(dir);
-        // transform.LookAt(hit.point);
-        Debug.DrawLine(transform.position, hit.point);
-    }
 }
---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+    public float maxHealth;
+    public float maxFuel;
+    [Tooltip("Единиц в секунду")]
+    public float fuelConsumption;
+    public Light lamp;
+
+    private float health;
+    private float fuelLevel;
+
+    private bool lightOn;
+
+    private void Start()
+    {
+        health = maxHealth;
+        fuelLevel = maxFuel;
+
+        lamp.gameObject.SetActive(false);
+        lightOn = false;
+    }
+
+    private IEnumerator FuelConsumption()
+    {
+        while (lightOn && fuelLevel > 0)
+        {
+            fuelLevel -= fuelConsumption;
+            yield return new WaitForSeconds(1);
+        }
+        // если топливо кончилось
+        Light(false);
+    }
+
+    public void Refuel(float fuel)
+    {
+        fuelLevel += fuel;
+        if (fuelLevel > maxFuel)
+        {
+            fuelLevel = maxFuel;
+        }
+    }
+
+    public void Damage(float dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Death();
+        }
+        else if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    private void Death()
+    {
+        Debug.Log("Вмер");
+        Destroy(gameObject);
+    }
+
+    private void Light(bool on)
+    {
+        lightOn = on;
+
+        if (lightOn)
+        {
+            if (fuelLevel > 0)
+            {
+                
+                StartCoroutine(FuelConsumption());
+            }
+            else
+            {
+                lightOn = false;
+            }
+        }
+        else
+        {
+            StopCoroutine(FuelConsumption());
+        }
+        lamp.gameObject.SetActive(lightOn);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            Light(!lightOn);
+        }
+    }
+
+}
